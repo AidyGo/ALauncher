a9e8002833feb060131a50901f95a8b5ac0b193e
mingxiao.li <mingxiao.li@tcl.com>
2014-12-03 19:32:34 +0800

    bottom drawer layout

--- src/com/aidy/launcher3/ui/settings/MenuFragment.java
+++ src/com/aidy/launcher3/ui/settings/MenuFragment.java
@@ -14,37 +14,34 @@ import android.view.WindowManager;
 
 public class MenuFragment extends DialogFragment {
 
-	private static MenuFragment mInstance;
+    private static MenuFragment mInstance;
 
-	private MenuFragment() {
-	}
+    public static MenuFragment getInstance() {
+        if (null == mInstance) {
+            mInstance = new MenuFragment();
+        }
+        return mInstance;
+    }
 
-	public static MenuFragment getInstance() {
-		if (null == mInstance) {
-			mInstance = new MenuFragment();
-		}
-		return mInstance;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        return inflater.inflate(R.layout.fragment_bottom_menu, null);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		return inflater.inflate(R.layout.fragment_bottom_menu, null);
-	}
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        // Window window = getDialog().getWindow();
+        // window.setGravity(Gravity.BOTTOM);
+        // WindowManager.LayoutParams layoutParams = window.getAttributes();
+        return super.onCreateDialog(savedInstanceState);
+    }
 
-	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		Window window = getDialog().getWindow();
-		window.setGravity(Gravity.BOTTOM);
-//		WindowManager.LayoutParams layoutParams = window.getAttributes();
-		return super.onCreateDialog(savedInstanceState);
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+    }
 
 }
